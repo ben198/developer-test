f013e9fc8ed5747d831fa53282c070078fc1d26a
Ben Townsend <btownsend220@gmail.com>
2016-09-29 17:32:47 +0100

    express now serves bundled assets from dist folder when get req is made to root

--- src/server.js
+++ src/server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('test test');
-});
+app.use(express.static('dist'));
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);

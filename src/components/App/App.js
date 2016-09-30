a1a8038c89381646b87195c60aa5d94f59f0ab82
Ben Townsend <btownsend220@gmail.com>
2016-09-30 11:15:19 +0100

    form placed within container. basic header and footer added to App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,18 @@ import Form from '../Form/Form';
 export default class App extends React.Component {
     render() {
         return (
-            <Form/>
+            <div>
+                <header className="header">
+                    <h1>Mubaloo Developer Test</h1>
+                </header>
+                <div className="container">
+                    <Form/>
+                </div>
+                <footer className="footer">
+                    <p>&copy;2016 Mubaloo</p>
+                    {/* I read the instructions */}
+                </footer>
+            </div>
         );
     }
 }

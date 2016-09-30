5cdee05c6e3509767a1472ab0ac6d413283b1ae2
Ben Townsend <btownsend220@gmail.com>
2016-09-30 12:20:17 +0100

    structure of form adjusted and responsive styles written for Form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,50 +3,62 @@ import React from 'react';
 export default class Form extends React.Component {
     render() {
         return (
-            <form action="/submit-survey" method="post">
+            <form className="form" action="/submit-survey" method="post">
                 <div>
                     <label for="title">Title:</label>
-                    <select id="title" name="user_title">
-                        <option>Mr</option>
-                        <option>Mrs</option>
-                        <option>Master</option>
-                        <option>Miss</option>
-                    </select>
+                    <div className="inputContainer">
+                        <select id="title" name="user_title">
+                            <option>Mr</option>
+                            <option>Mrs</option>
+                            <option>Master</option>
+                            <option>Miss</option>
+                        </select>
+                    </div>
                 </div>
                 <div>
                     <label for="name">Name:</label>
-                    <input 
-                        type="text"
-                        id="name" 
-                        name="user_name"/>
+                    <div className="inputContainer">
+                        <input
+                            type="text"
+                            id="name"
+                            name="user_name"/>
+                    </div>
                 </div>
                 <div>
                     <label for="dateOfBirth">Date of Birth:</label>
-                    <input 
-                        type="date"
-                        id="dateOfBirth" 
-                        name="user_dob"/>
+                    <div className="inputContainer">
+                        <input
+                            type="date"
+                            id="dateOfBirth"
+                            name="user_dob"/>
+                    </div>
                 </div>
                 <div>
                     <label for="currentLocation">Current Location:</label>
-                    <input 
-                        type="text"
-                        id="currentLocation" 
-                        name="user_location"/>
+                    <div className="inputContainer">
+                        <input
+                            type="text"
+                            id="currentLocation"
+                            name="user_location"/>
+                    </div>
                 </div>
                 <div>
                     <label for="currentDateAndTime">Current Date/Time:</label>
-                    <input 
-                        type="datetime-local"
-                        id="currentDateAndTime" 
-                        name="user_dateAndTime"/>
+                    <div className="inputContainer">
+                        <input
+                            type="datetime-local"
+                            id="currentDateAndTime"
+                            name="user_dateAndTime"/>
+                    </div>
                 </div>
                 <div>
                     <label for="feedback">User Feedback:</label>
-                    <textarea 
-                        id="feedback" 
-                        name="user_feedback">
-                    </textarea>
+                    <div className="inputContainer">
+                        <textarea
+                            id="feedback"
+                            name="user_feedback">
+                        </textarea>
+                    </div>
                 </div>
                 <div>
                     <button type="submit">Submit</button>
